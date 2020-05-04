84e38c49f877c12d3d06db152a82a7740ccd9a7b
Takamitsu Mizutori <mizutori@gmail.com>
2020-05-04 12:32:14 +0900

    for Tutorial 1-1

--- app/src/main/java/com/goldrushcomputing/databindingtutorial/MainActivity.kt
+++ app/src/main/java/com/goldrushcomputing/databindingtutorial/MainActivity.kt
@@ -7,12 +7,12 @@ import com.goldrushcomputing.databindingtutorial.databinding.ActivityMainBinding
 import com.goldrushcomputing.databindingtutorial.model.User
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         val user = User("Benjamin")
         binding.user = user
     }
